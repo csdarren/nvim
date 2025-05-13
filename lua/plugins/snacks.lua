860d5eaea0b34eb2c27d58c4f7f9df4af064de6d
csdarren <2001dhall@gmail.com>
2025-05-13 14:37:44 -0400

    Added floating terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,36 @@ return {
     "folke/snacks.nvim",
     ---@type snacks.Config
     opts = {
+        styles = {
+            c_term = {
+                backdrop = false,
+                position = "float",
+                border = "rounded",
+                title_pos = "center",
+                height = 0.8,
+                width = 0.8,
+                relative = "editor",
+                noautocmd = true,
+                wo = {
+                    winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+                    cursorline = false,
+                },
+                b = {
+                    completion = false, -- disable blink completions in input
+                },
+                keys = {
+                    n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+                    i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
+                    i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
+                    i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+                    i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+                    i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+                    i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+                    q = "cancel",
+                },
+            },
+        },
+        terminal = { win = { style = "c_term" } },
         dashboard = {
             preset = {
                 header = [[
@@ -20,8 +50,6 @@ https://github.com/csdarren/
         picker = {
             sources = {
                 explorer = {
-                    -- make all files that start with . visible at all times (same thing has pressing 'H' in default explorer. )
-                    -- include = { ".*" },
                     win = {
                         list = {
                             keys = { ["-"] = "explorer_up" },
